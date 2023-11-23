cf8710127faf5598a76647493ee53511357d8c95
Marius <52743525+avolgha@users.noreply.github.com>
2023-11-23 17:27:45 +0100

    Fixed ToDo with index.html file for directories.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Simple web server you can start in milliseconds!
 > Would love to receive PRs with additional features!
 
 - [ ] when requesting directory:
-  - [ ] search for `index.html` file
+  - [X] search for `index.html` file
   - [ ] otherwise show Directory Browser
 - [ ] specifiable 404-error page
 - [X] add `renderMarkdown`-function at [src/bin.ts L26](https://github.com/avolgha/up/blob/dev/src/bin.ts#L26)
--- src/renderDirectory.ts
+++ src/renderDirectory.ts
@@ -1,7 +1,16 @@
 import log from "@avolgha/log";
+import fs from "fs";
+import path from "path";
 import { RenderFunction } from "./types";
+import renderDocument from "./renderDocument";
 
-const renderDirectory: RenderFunction = (_req, res) => {
+const renderDirectory: RenderFunction = (req, res, dirname, opts) => {
+	const indexFilePath = path.resolve(dirname, "index.html");
+	if (fs.existsSync(indexFilePath)) {
+		renderDocument(req, res, indexFilePath, opts);
+		return;
+	}
+
 	log.debug("TODO: make directories readable.");
 	res.writeHead(404);
 	res.write("Error: Directories are not readable currently.");
